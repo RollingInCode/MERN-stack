04e5a460fccdc5ddbefce02dcf2ec1ea3a8e2041
Franklyn Gonzalez <31908148+RollingInCode@users.noreply.github.com>
2021-01-09 20:00:34 -0500

    Add files via upload

---
+++ build/precache-manifest.37815e248f0c0eaaa03fad17b0abc757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debcd810c6a77793de73ff3bcd366a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316d3c5633a417e5478c",
+    "url": "/static/css/2.293e860e.chunk.css"
+  },
+  {
+    "revision": "316d3c5633a417e5478c",
+    "url": "/static/js/2.af0a3201.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/static/js/2.af0a3201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e10b99e9d5f786b6df",
+    "url": "/static/js/main.144e4168.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
